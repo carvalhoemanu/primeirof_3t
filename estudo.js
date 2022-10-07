d763f63bb4423cb03eb1611aad68797a34799ed6
carvalhoemanu <108936688+carvalhoemanu@users.noreply.github.com>
2022-10-07 09:58:33 -0300

    aula 07/10

--- estudo.js
+++ estudo.js
@@ -33,6 +33,66 @@ function mostra(){
 }
 function mostraTabuadafor() {
      for (let i = 1; i <= 10; i++
+          
+          let tabuada = 9;
+
+function escreva(){
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada *1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada *2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada *3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada *4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada *5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada *6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada *7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada *8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada *9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada *10) + "<br>");
+    
+
+    
+
+
+}
+
+let lista = ["Jesus","Duarte","Luciana","jorge"]
+
+function mostra(){
+    document.write(lista.length + "<br>");
+    for (let i = 0; i < 7; i++) {
+
+    }
+    document.write(lista[0] + "<br>");
+         
+    
+
+}
+function mostraTabuadafor() {
+     for (let i = 1; i <= 10; i++){
+         document.write("o valor do i " + i + "<br>");
+     }
+         
+     }
+function total (){
+    <button> onclick ="total()>Enviar</button>
+   letra v = document,getElementById("valor").value;
+   letra j = document,getElementById("juros").value;
+   letra t = document,getElementById("meses").value;
+   letra c = document,getElementById("capital").value;
+   letra r = 0;
+   for (let i =1; 1 <=t; i++){
+   let r = v * (1 +(j/100));
+   document.write("Mês" + i + "-valor:"+ r + "<br>" );
+   v = r;
+   }
+
+   document.write("Resultado:" + r);
+
+}
+
+
+
+
          
      }
 
